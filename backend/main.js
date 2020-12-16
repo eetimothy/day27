b84db779b60f9b239cf8ab089ae8ae776dae1bd4
eetimothy <eetimothy@gmail.com>
2020-12-16 21:49:10 +0800

    upd

--- backend/main.js
+++ backend/main.js
@@ -10,24 +10,57 @@ const cors = require('cors')
 const morgan = require('morgan')
 const Timestamp = require('mongodb').Timestamp
 const fs = require('fs') //to delete file
+const { S3 } = require('aws-sdk')
 
 
 const DATABASE_NAME = 'take-temp-tgt' //do not type out db/ collection names, use const instead
 const COLLECTION = 'temperature'
 
 //construct outside 'PORT' to minimise error
-const mkTemperature = (params) => {
+const mkTemperature = (params, image) => {
     return {
         ts: new Date(),
         user: params.userName,
         q1: params.q1,
         q2: params.q2,
-        temperature: params.temperature
+        temperature: parseFloat(params.temperature),
+        image
     }
 }
 
+const readFile = (path) => new Promise(
+    (resolve, reject) =>
+        fs.readFile(path, (err, buff) => {
+            if (null != err)
+                reject(err)
+            else
+                resolve(buff)
+        })
+
+)
+
+const putObject = (file, buff, s3) => new Promise(
+    (resolve, reject) => {
+        const params = {
+            Bucket: '',
+            Key: file.filename,
+            Body: buff,
+            ACL: 'public read',
+            ContentType: file.mimetype,
+            ContentLength: file.size
+        }
+        S3.putObject(params, (err, result) => {
+            if (null != err)
+                reject(err)
+            else
+                resolve(result)
+        })
+    }
+)
+
 //import MongoDb driver
-const MongoClient = require('mongodb').MongoClient
+const { MongoClient } = require('mongodb')
+const { RSA_NO_PADDING } = require('constants')
 
 //connection string
 const MONGO_URL = 'mongodb://localhost:27017'
@@ -55,27 +88,11 @@ const s3 = new aws.S3({
     secretAccessKey: AWS_S3_SECRET_ACCESSKEY
 })
 
-
 const upload = multer({
     //dest: process.env.TMP_DIR || './uploads' //place in temp folder
-    storage: multerS3({
-        s3: s3,
-        bucket: AWS_S3_BUCKET_NAME,
-        acl: 'public read',
-        metadata: function (req, file, cb) {
-            cb(null, {
-                filename: file.filename,
-                orignialFileName: file.originalname,
-                 
-            })
-        },
-        key: function (request, file, cb) {
-            console.log(file)
-            cb(null, file.originalname)
-        },
-        limits: { fileSize: 2000000 },
-    })
+    dest: process.env.TMP_DIR || './uploads'
 })
+
 
 //initiate express
 const app = express()
@@ -93,61 +110,77 @@ app.post('/temperature', upload.single('temp-img'), (req, res) => {
     //req.body.userName, 
     //req.body.q1, req.body.q2, 
     //req.body.temperature
-    const doc = mkTemperature(req.body)
-
     res.on('finish', () => {
         //delete the temp file
         fs.unlink(req.file.path, () => { })
         //console.info('>>> response ended')
     })
-    console.info('>>> req.body: ', req.body)
-    console.info('>>> req.file: ', req.file)
-
-    res.status(200)
-    res.json({})
+    //console.info('>>> req.body: ', req.body)
+    //console.info('>>> req.file: ', req.file)
+    const doc = mkTemperature(req.body)
 
     // TODO: insert doc into mongo
+    /*
+        mongoClient.db(DATABASE_NAME)
+            .collection(COLLECTION)
+            .insertOne(doc)
+            .then(result => {
+                console.info('insert result: ', results)
+                res.status(200)
+                // res.type('application/json')
+                res.json({})
+            }).catch(error => {
+                console.info('insert error: ', error)
+                res.status(500)
+                res.json({ error })
+            })
     
-    mongoClient.db(DATABASE_NAME)
-    .collection(COLLECTION)
-    .insertOne(doc)
-    .then(result => {
-        console.info('insert result: ', results)
+    
         res.status(200)
-       // res.type('application/json')
+        res.type('application/json')
         res.json({})
-    }).catch(error => {
-        console.info('insert error: ', error)
-        res.status(500)
-        res.json({error})
+    
+    })
+    */
+    readFile(req.file.path)
+        .then(buff =>
+            putObject(req.file, buff, s3)
+        )
+        .then(() =>
+            mongoClient.db(DATABASE_NAME).collection(COLLECTION)
+                .insertOne(doc)
+        )
+        .then(results => {
+            console.ingo('insert results: ', results)
+            res.status(200)
+            res.json({ id: results.ops[0]._id })
+        })
+        .catch(error => {
+            console.error('insert error: ', error)
+            res.status(500)
+            res.json({ error })
+        })
     })
 
-
-    res.status(200)
-    res.type('application/json')
-    res.json({})
-
-})
-
-//keys must be available and must connect mongodb before startin app
-//construct a promise
-const p0 = new Promise(
-    (resolve, reject) => {
-        if ((!!process.env.AWS_S3_ACCESSKEY_ID) && (!!process.env.AWS_S3_SECRET_ACCESSKEY)) {
-            resolve()
-        } else {
-            reject('S3 keys not found')
+    //keys must be available and must connect mongodb before startin app
+    //construct a promise
+    const p0 = new Promise(
+        (resolve, reject) => {
+            if ((!!process.env.AWS_S3_ACCESSKEY_ID) && (!!process.env.AWS_S3_SECRET_ACCESSKEY)) 
+                resolve()
+             else 
+                reject('S3 keys not found')
+            
         }
-    }
 
-)
-const p1 = mongoClient.connect()
-//start server
-Promise.all([[p0, p1]])
-    .then(() => {
-        app.listen(PORT, () => {
-            console.info(`Application started on ${PORT} at ${new Date()}`)
+    )
+    const p1 = mongoClient.connect()
+    //start server
+    Promise.all([[p0, p1]])
+        .then(() => {
+            app.listen(PORT, () => {
+                console.info(`Application started on ${PORT} at ${new Date()}`)
+            })
         })
-    }).catch(e => {
-        console.error('Unable to connect to DB')
-    }) 
+        
+        .catch(err => { console.error('Cannot connect: ', err) })
